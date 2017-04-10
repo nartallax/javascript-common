accb382eb16f341e4196b8a208fe03d3f14ac324
Nartallax <nartallax@yandex-team.ru>
2017-04-10 17:20:28 +0300

    Async stream finished untested

--- template/app.js
+++ template/app.js
@@ -6,26 +6,17 @@ require('../../javascript-common/libs/meta/addict.js')
 		pkg('global')();
 		
 		var getCallStack = pkg('meta.stack');
+				
+		var stream = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].async();
+		stream.groupBy((a, cb) => cb(~~(a / 5))).map((x, cb) => x.array(cb)).array(x => log(x));
 		
-		var stream = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-			.async()
-			.flatMap((x, cb) => {
-				log('running for ' + x);
-				setTimeout(() => {
-					log('done for ' + x);
-					cb([x, x * x, x * x * x])
-				}, ~~(Math.random() * 500))
-			}, 3, 3)
-			.filter((x, cb) => cb(x % 2))
-			
-		stream.array(arr => log(arr));
-		stream.array(arr => log(arr));
+		
 		/*
 		var stream = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 			.flatMap(x => [x, x * x, x * x * x])
 			.filter(x => x % 2);
 		
-		log(stream.array());
+		log(stream.tail(10).array());
 		*/
 		
 	});
