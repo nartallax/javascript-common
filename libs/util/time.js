7374a2e1d69f24af9dc6667bb08ed8867c2c9663
Nartallax <nartallax@gmail.com>
2018-08-04 22:50:39 +0300

    New time formatting function

--- libs/util/time.js
+++ libs/util/time.js
@@ -2,11 +2,17 @@ pkg('util.time', () => {
 	var twoDigits = n => n > 9? n + '': '0' + n,
 		threeDigits = n => n > 99? n + '': '0' + twoDigits(n);
 		
+	let dateOf = d => typeof(d) === "number"? new Date(d): d && (d instanceof Date)? d: null;
+		
 	var timeToString = d => twoDigits(d.getHours()) + ':' + twoDigits(d.getMinutes()) + ':' + twoDigits(d.getSeconds())
 	var dateToString = d => d.getFullYear() + '.' + twoDigits(d.getMonth() + 1) + '.' + twoDigits(d.getDate())
 	var formatDateTime = d => {
-		d || (d = new Date());
+		d = dateOf(d) || new Date()
 		return dateToString(d) + ' ' + timeToString(d) + ' ' + threeDigits(d.getMilliseconds());
+	}
+	let formatUnixtimeToDateTimeMinutesOrEmpty = d => {
+		d = dateOf(d * 1000);
+		return !d? "": dateToString(d) + " " + twoDigits(d.getHours()) + ":" + twoDigits(d.getMinutes())
 	}
 	var formatDateTimeSeconds = d => {
 		d || (d = new Date());
@@ -19,6 +25,7 @@ pkg('util.time', () => {
 		unixtimeToDate: u => new Date(u * 1000),
 		millisecondsToDate: m => new Date(m),
 		formatDate: formatDateTime,
+		formatUnixtimeToDateTimeMinutesOrEmpty: formatUnixtimeToDateTimeMinutesOrEmpty, 
 		formatUnixtime: u => formatDateTimeSeconds(new Date(u * 1000)),
 		formatMilliseconds: m => formatDateTime(new Date(m)),
 		nowString: formatDateTime,
